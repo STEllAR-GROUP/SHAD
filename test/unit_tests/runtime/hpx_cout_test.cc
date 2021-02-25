0b4236856384ca37e07605949ed043c86c5aa1f3
nanmiao <nanmiaowu@gmail.com>
2021-02-25 16:45:08 -0600

    a simple hpx cout test fails

--- test/unit_tests/runtime/hpx_cout_test.cc
+++ test/unit_tests/runtime/hpx_cout_test.cc
@@ -1,18 +1,16 @@
-#include <hpx/config.hpp>
-#include <hpx/hpx.hpp>
-#include <hpx/hpx_init.hpp>
+#include "gtest/gtest.h"
+
+#include "shad/runtime/runtime.h"
 
 #include <hpx/iostream.hpp>
+#include <iostream>
 
-int hpx_main()
-{
+namespace shad{
 
-    hpx::cout << "Hello HPX World!" << std::endl;
-
-    return hpx::finalize();
-}
 int main(int argc, char* argv[])
 {
-    std::cout << "running HPX test " << std::endl;
-    return hpx::init(argc, argv);       // Initialize and run HPX.
+    std::cout << "Hello World! \n";
+    return 0;
 }
+
+} //namespace shad
