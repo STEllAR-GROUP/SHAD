02422581b9caf19ac044dcb74a7d1122792ef4ab
nanmiaowu <nanmiaowu@gmail.com>
2021-06-04 17:35:16 -0500

    array: adding count, find_if, for_each, minmax, transform shad algorithms

--- examples/tutorial/tutorial.cc
+++ examples/tutorial/tutorial.cc
@@ -32,8 +32,9 @@
 #include "shad/core/unordered_set.h"
 
 constexpr static size_t kArraySize = 4;
+using iterator = shad::impl::array<int, kArraySize>::array_iterator<int>;
 
-void shad_generate_algirthm(shad::array<int, kArraySize> &in){
+void shad_generate_algorithm(shad::array<int, kArraySize> &in){
   shad::generate(
     shad::distributed_parallel_tag{},
     in.begin(), in.end(), 
@@ -46,32 +47,112 @@ void shad_generate_algirthm(shad::array<int, kArraySize> &in){
   );
 }
 
-void shad_fill_algirthm(shad::array<int, kArraySize> &in){
+void shad_fill_algorithm(shad::array<int, kArraySize> &in){
   shad::fill(
     shad::distributed_parallel_tag{},
     in.begin(), in.end(), 42);
+}
+
+size_t shad_count_algorithm(shad::array<int, kArraySize> &in){
+  auto counter = shad::count(
+                    shad::distributed_parallel_tag{},
+                    in.begin(), in.end(), 1);
+  return counter;
+}
+
+iterator shad_find_if_algorithm(shad::array<int, kArraySize> &in){
+  auto iter = shad::find_if(
+                    shad::distributed_parallel_tag{},
+                    in.begin(), in.end(),
+                    [](int i){ return i%2 == 0;});
+  return iter;
+}
+
+void shad_for_each_algorithm(shad::array<int, kArraySize> &in){
+  shad::for_each(
+    shad::distributed_parallel_tag{},
+    in.begin(), in.end(),
+    [](int& i){ i++; });
+}
+
+std::pair<iterator, iterator> shad_minmax_algorithm(shad::array<int, kArraySize> &in){
+  auto [min, max] = shad::minmax_element(
+                    shad::distributed_parallel_tag{},
+                    in.begin(), in.end());
+  return {min, max};
+}
+
+iterator shad_transform_algorithm(shad::array<int, kArraySize> &in){
+  auto iter = shad::transform(
+                    shad::distributed_parallel_tag{},
+                    in.begin(), in.end(), in.begin(),
+                    [](int i){ return i++;});
+  return iter;
 }
 
 namespace shad {
 
 int main(int argc, char *argv[]) {
 
+  // array
   shad::array<int, kArraySize> in;
 
   // shad fill algorithm
   auto execute_time = shad::measure<std::chrono::nanoseconds>::duration(
-    [&](){shad_fill_algirthm(in);});
-  std::cout << "Using " << shad::rt::numLocalities() 
-            << " localities, shad fill of array took " 
-            << execute_time.count() << "nanoseconds" << std::endl;
+    [&](){shad_fill_algorithm(in);});
+  std::cout << "Array, using " << shad::rt::numLocalities() 
+            << " localities, shad::fill took " 
+            << execute_time.count() << " nanoseconds" << std::endl;
 
   // shad generate algorithm
   execute_time = shad::measure<std::chrono::nanoseconds>::duration(
-    [&](){shad_generate_algirthm(in);});
-  std::cout << "Using " << shad::rt::numLocalities() 
-            << " localities, shad generate of array took " 
-            << execute_time.count() << "nanoseconds" << std::endl;
+    [&](){shad_generate_algorithm(in);});
+  std::cout << "Array, using " << shad::rt::numLocalities() 
+            << " localities, shad::generate took " 
+            << execute_time.count() << " nanoseconds" << std::endl;
 
+  // shad count algorithm
+  size_t counter;
+  execute_time = shad::measure<std::chrono::nanoseconds>::duration(
+    [&](){counter = shad_count_algorithm(in);});
+  std::cout << "Array, using " << shad::rt::numLocalities() 
+            << " localities, shad::count took " 
+            << execute_time.count() << " nanoseconds (numbers of 0 = "
+            << counter << " )" << std::endl;
+
+  // shad find_if algorithm
+  iterator iter;
+  execute_time = shad::measure<std::chrono::nanoseconds>::duration(
+    [&](){iter = shad_find_if_algorithm(in);});
+  std::cout << "Array, using " << shad::rt::numLocalities() 
+            << " localities, shad::find_if took " 
+            << execute_time.count() << " nanoseconds, ";
+  (iter != in.end()) 
+    ? std::cout << "array contains an even number" << std::endl
+    : std::cout << "array does not contain even numbers" << std::endl;
+
+  // shad for_each algorithm
+  execute_time = shad::measure<std::chrono::nanoseconds>::duration(
+    [&](){shad_for_each_algorithm(in);});
+  std::cout << "Array, using " << shad::rt::numLocalities() 
+            << " localities, shad::for_each took " 
+            << execute_time.count() << " nanoseconds" << std::endl;
+
+  // shad minmax algorithm
+  std::pair<iterator, iterator> min_max;
+  execute_time = shad::measure<std::chrono::nanoseconds>::duration(
+    [&](){ min_max = shad_minmax_algorithm(in); });
+  std::cout << "Array, using " << shad::rt::numLocalities() 
+            << " localities, shad::count took " 
+            << execute_time.count() << " nanoseconds (min = "
+            << *min_max.first<< ", max = " << *min_max.second << " )" << std::endl;
+
+  // shad transform algorithm
+  execute_time = shad::measure<std::chrono::nanoseconds>::duration(
+    [&](){iter = shad_transform_algorithm(in);});
+  std::cout << "Array, using " << shad::rt::numLocalities() 
+            << " localities, shad::transform took " 
+            << execute_time.count() << " nanoseconds" << std::endl;
   
   return 0;
 }
