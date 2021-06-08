c3056f6563cc0621d06228dbcaaf277a3d679612
nanmiaowu <nanmiaowu@gmail.com>
2021-06-08 12:22:53 -0500

    adding shad find_if, any of, count if for unordered_set example

--- examples/tutorial/unordered_set.cc
+++ examples/tutorial/unordered_set.cc
@@ -23,11 +23,7 @@
 //===----------------------------------------------------------------------===/
 
 #include <iostream>
-#include <random>
-#include <stdlib.h>
 
-
-#include "shad/core/array.h"
 #include "shad/core/algorithm.h"
 #include "shad/util/measure.h"
 #include "shad/core/unordered_set.h"
@@ -37,49 +33,6 @@ using iterator = shad::Set<int>::iterator;
 using shad_inserter_t = shad::insert_iterator<shad::unordered_set<int>>;
 using shad_buffered_inserter_t = shad::buffered_insert_iterator<shad::unordered_set<int>>;
 
-/***
-void shad_generate_algorithm(shad::array<int, kSize> &in){
-  shad::generate(
-    shad::distributed_parallel_tag{},
-    in.begin(), in.end(), 
-    [=]() {
-        std::random_device rd;
-        std::default_random_engine G(rd());
-        std::uniform_int_distribution<int> dist(1, 10);
-        return dist(G);
-    }
-  );
-}
-
-void shad_fill_algorithm(shad::array<int, kSize> &in){
-  shad::fill(
-    shad::distributed_parallel_tag{},
-    in.begin(), in.end(), 42);
-}
-
-size_t shad_count_algorithm(shad::array<int, kSize> &in){
-  auto counter = shad::count(
-                    shad::distributed_parallel_tag{},
-                    in.begin(), in.end(), 1);
-  return counter;
-}
-
-iterator shad_find_if_algorithm(shad::array<int, kSize> &in){
-  auto iter = shad::find_if(
-                    shad::distributed_parallel_tag{},
-                    in.begin(), in.end(),
-                    [](int i){ return i%2 == 0;});
-  return iter;
-}
-
-void shad_for_each_algorithm(shad::array<int, kSize> &in){
-  shad::for_each(
-    shad::distributed_parallel_tag{},
-    in.begin(), in.end(),
-    [](int& i){ i++; });
-}
-***/
-
 std::pair<iterator, iterator> shad_minmax_algorithm(shad::unordered_set<int> &in){
   auto [min, max] = shad::minmax_element(
                     shad::distributed_parallel_tag{},
@@ -87,14 +40,37 @@ std::pair<iterator, iterator> shad_minmax_algorithm(shad::unordered_set<int> &in
   return {min, max};
 }
 
+iterator shad_find_if_algorithm(shad::unordered_set<int> &in){
+  auto iter = shad::find_if(
+                    shad::distributed_parallel_tag{},
+                    in.begin(), in.end(),
+                    [](int i){ return i%2 == 0;});
+  return iter;
+}
 
+bool shad_any_of_algorithm(shad::unordered_set<int> &in){
+  auto res = shad::any_of(
+                    shad::distributed_parallel_tag{},
+                    in.begin(), in.end(),
+                    [](int i){ return i%7 == 0;});
+  return res;
+}
+
+size_t shad_count_if_algorithm(shad::unordered_set<int> &in){
+  auto counter = shad::count_if(
+                    shad::distributed_parallel_tag{},
+                    in.begin(), in.end(), 
+                    [](int i){return i % 3 == 0;});
+  return counter;
+}
+
+template<typename shad_inserter>
 void shad_transform_algorithm(shad::unordered_set<int> &in){
   shad::unordered_set<int> out;
   shad::transform(
                     shad::distributed_parallel_tag{},
-                    in.begin(), in.end(), shad_inserter_t(out, out.begin()),
+                    in.begin(), in.end(), shad_inserter(out, out.begin()),
                     [](const std::unordered_set<int>::value_type &i){ return i;});
-  //for(auto v : out) std::cout<< v << std::endl;
 }
 
 namespace shad {
@@ -107,7 +83,7 @@ int main(int argc, char *argv[]) {
   // create set
   shad_inserter_t ins(set_, set_.begin());
   for (size_t i = 0; i < kSize; ++i){
-    ins = 2 * i;
+    ins = 2 * (i + 1);
   } 
 
   // shad minmax algorithm
@@ -119,10 +95,43 @@ int main(int argc, char *argv[]) {
             << execute_time.count() << " nanoseconds (min = "
             << *min_max.first<< ", max = " << *min_max.second << " )" << std::endl;
 
-  // shad transform algorithm
-
+  // shad find_if algorithm
+  iterator iter;
   execute_time = shad::measure<std::chrono::nanoseconds>::duration(
-    [&](){shad_transform_algorithm(set_);});
+    [&](){iter = shad_find_if_algorithm(set_);});
+  std::cout << "Unordered set, using " << shad::rt::numLocalities() 
+            << " localities, shad::find_if took " 
+            << execute_time.count() << " nanoseconds, ";
+  (iter != set_.end()) 
+    ? std::cout << "Unordered set contains an even number" << std::endl
+    : std::cout << "Unordered set does not contain even numbers" << std::endl;
+
+
+  // shad any_of algorithm
+  bool res;
+  execute_time = shad::measure<std::chrono::nanoseconds>::duration(
+    [&](){res = shad_any_of_algorithm(set_);});
+  std::cout << "Unordered set, using " << shad::rt::numLocalities() 
+            << " localities, shad::any_of took " 
+            << execute_time.count() << " nanoseconds, ";
+  (res == true) 
+    ? std::cout << "and this unordered set contains at least one number that is divisible by 7" << std::endl
+    : std::cout << "and this unordered set does not contain any number that is divisible by 7" << std::endl;
+
+
+  // shad count_if algorithm
+  size_t counter;
+  execute_time = shad::measure<std::chrono::nanoseconds>::duration(
+    [&](){counter = shad_count_if_algorithm(set_);});
+  std::cout << "Unordered set, using " << shad::rt::numLocalities() 
+            << " localities, shad::count_if took " 
+            << execute_time.count() << " nanoseconds, "
+            << "and number divisible by 3: " << counter << std::endl;
+
+
+  // shad transform algorithm
+  execute_time = shad::measure<std::chrono::nanoseconds>::duration(
+    [&](){shad_transform_algorithm<shad_inserter_t>(set_);});
   std::cout << "Unordered set, using " << shad::rt::numLocalities() 
             << " localities, shad::transform took " 
             << execute_time.count() << " nanoseconds" << std::endl;
